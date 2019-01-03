612fa8f96c63de6ad96dc17687b15bee7f007234
df5602 <dominik@fankhausers.ch>
2019-01-03 14:44:46 +0100

    add testcase for default initialization

--- tests/structs.rs
+++ tests/structs.rs
@@ -60,7 +60,10 @@ fn derive_nested_struct_construct_with() {
     impl InnerRectangle {
         fn new(x: usize, y: usize, width: usize, height: usize) -> Self {
             Self {
-                x, y, width, height
+                x,
+                y,
+                width,
+                height,
             }
         }
     }
@@ -79,4 +82,25 @@ fn derive_nested_struct_construct_with() {
     assert_eq!(2, rect.rect.y);
     assert_eq!(5, rect.rect.width);
     assert_eq!(4, rect.rect.height);
+}
+
+#[test]
+fn derive_struct_with_default_initialization() {
+    #[derive(FromStr)]
+    #[adhoc(regex = r"^(?P<x>\d+),(?P<y>\d+): (?P<width>\d+)x(?P<height>\d+)$")]
+    struct Rectangle {
+        #[adhoc(construct_with = "Default::default()")]
+        id: usize,
+        x: usize,
+        y: usize,
+        width: usize,
+        height: usize,
+    }
+
+    let rect: Rectangle = "3,2: 5x4".parse::<Rectangle>().unwrap();
+    assert_eq!(0, rect.id);
+    assert_eq!(3, rect.x);
+    assert_eq!(2, rect.y);
+    assert_eq!(5, rect.width);
+    assert_eq!(4, rect.height);
 }
