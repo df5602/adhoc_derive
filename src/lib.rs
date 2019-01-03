9bc6e2c16f734c3abaf6ac79276d776dd61531d0
df5602 <dominik@fankhausers.ch>
2019-01-03 16:12:14 +0100

    add example to crate root

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,28 @@
+//! This crate allows you to derive a `std::str::FromStr` implementation based on a regex
+//! provided via macro attribute.
+//!
+//! ```edition2018
+//! use adhoc_derive::FromStr;
+//! #[derive(FromStr)]
+//! #[adhoc(regex = r"^#(?P<id>\d+) @ (?P<x>\d+),(?P<y>\d+): (?P<width>\d+)x(?P<height>\d+)$")]
+//! struct Rectangle {
+//!     id: usize,
+//!     x: usize,
+//!     y: usize,
+//!     width: usize,
+//!     height: usize,
+//! }
+//!
+//! let rect: Rectangle = "#123 @ 3,2: 5x4".parse().unwrap();
+//! assert_eq!(123, rect.id);
+//! assert_eq!(3, rect.x);
+//! assert_eq!(2, rect.y);
+//! assert_eq!(5, rect.width);
+//! assert_eq!(4, rect.height);
+//! ```
+//!
+//! Refer to [GUIDE.md](https://github.com/df5602/adhoc_derive/blob/master/GUIDE.md) for more examples.
+
 #![recursion_limit = "128"]
 
 extern crate proc_macro;
